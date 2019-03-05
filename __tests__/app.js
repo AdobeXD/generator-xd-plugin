3551e7621c20a865624416a334058dc6dd422c29
Yoshiki Takeoka <yoshikinoko@gmail.com>
2019-03-05 17:28:03 +0900

    update tests

--- __tests__/app.js
+++ __tests__/app.js
@@ -1,23 +1,66 @@
 'use strict';
+
+const answers = require('../generators/prompts/answers');
 const path = require('path');
 const assert = require('yeoman-assert');
 const helpers = require('yeoman-test');
 
-const noui = `Plugin with No UI`;
-
 describe('generator-xd-plugin:app', () => {
-    beforeAll(() => {
-        return helpers
-            .run(path.join(__dirname, '../generators/app'))
-            .withPrompts({ name: 'Hello' })
-            .withPrompts({ scriptType: noui });
+  Object.values(answers.version).forEach(version => {
+    // Regex for /"minVersion"\s*:\s*"16\.0.\d"/
+    const _manifestMinVersion = new RegExp(
+      `"minVersion"\\s*:\\s*"${version}.\\d"`.replace('.', '\\.')
+    );
+
+    it(`generating template of plugin with no dialog for v${version}`, () => {
+      return helpers
+        .run(path.join(__dirname, '../generators/app'))
+        .withPrompts({ name: answers.projectDisplayName })
+        .withPrompts({ minVersion: version })
+        .withPrompts({ scriptType: answers.framework.noui.dialog })
+        .then(() => {
+          assert.file(['static/manifest.json']);
+          assert.file(['static/images/icon.png']);
+          assert.file(['package.json']);
+          assert.file(['README.md']);
+          assert.file(['src/main.js']);
+          // Check minVersion of manifest.json
+          assert.fileContent('static/manifest.json', _manifestMinVersion);
+        });
     });
 
-    it('creates files', () => {
-        assert.file(['src/main.js']);
-        assert.file(['static/manifest.json']);
-        assert.file(['static/images/icon.png']);
-        assert.file(['package.json']);
-        assert.file(['README.md']);
+    it(`generating template of plugin with dialog of react for v${version}`, () => {
+      return helpers
+        .run(path.join(__dirname, '../generators/app'))
+        .withPrompts({ name: answers.projectDisplayName })
+        .withPrompts({ minVersion: version })
+        .withPrompts({ scriptType: answers.framework.react.dialog })
+        .then(() => {
+          assert.file(['static/manifest.json']);
+          assert.file(['static/images/icon.png']);
+          assert.file(['package.json']);
+          assert.file(['README.md']);
+          assert.file(['src/main.jsx']);
+          // Check minVersion of manifest.json
+          assert.fileContent('static/manifest.json', _manifestMinVersion);
+        });
     });
+
+    it(`generating template of plugin with dialog of plain js for v${version}`, () => {
+      return helpers
+        .run(path.join(__dirname, '../generators/app'))
+        .withPrompts({ name: answers.projectDisplayName })
+        .withPrompts({ minVersion: version })
+        .withPrompts({ scriptType: answers.framework.plain.dialog })
+        .then(() => {
+          assert.file(['static/manifest.json']);
+          assert.file(['static/images/icon.png']);
+          assert.file(['package.json']);
+          assert.file(['README.md']);
+          assert.file(['src/main.js']);
+          // Check minVersion of manifest.json
+          assert.fileContent('static/manifest.json', _manifestMinVersion);
+        });
+    });
+  });
 });
