e0c8c35c3d7e9f1c7b132db63e09783cea6ff012
Yoshiki Takeoka <yoshikinoko@gmail.com>
2019-09-17 19:53:14 +0900

    updated test cases.

--- __tests__/app.js
+++ __tests__/app.js
@@ -12,14 +12,20 @@ describe('generator-xd-plugin:app', () => {
       .withPrompts({ name: answers.projectDisplayName })
       .withPrompts({ scriptType: answers.framework.noui.dialog })
       .then(() => {
+        assert.file(['.editorconfig']);
+        assert.file(['.eslintignore']);
+        assert.file(['.eslintrc']);
+        assert.file(['.gitignore']);
+        assert.file(['README.md']);
+        assert.file(['package.json']);
+        assert.file(['webpack.config.js']);
+        assert.file(['src/main.js']);
         assert.file(['static/manifest.json']);
+        assert.file(['static/images/icon@0.5x.png']);
         assert.file(['static/images/icon@1x.png']);
         assert.file(['static/images/icon@2x.png']);
         assert.file(['static/images/icon@3x.png']);
         assert.file(['static/images/icon@4x.png']);
-        assert.file(['package.json']);
-        assert.file(['README.md']);
-        assert.file(['src/main.js']);
       });
   });
 
@@ -29,14 +35,23 @@ describe('generator-xd-plugin:app', () => {
       .withPrompts({ name: answers.projectDisplayName })
       .withPrompts({ scriptType: answers.framework.react.dialog })
       .then(() => {
+        assert.file(['.editorconfig']);
+        assert.file(['.eslintignore']);
+        assert.file(['.eslintrc']);
+        assert.file(['.gitignore']);
+        assert.file(['.stylelintrc']);
+        assert.file(['README.md']);
+        assert.file(['package.json']);
+        assert.file(['webpack.config.js']);
+        assert.file(['src/main.jsx']);
+        assert.file(['src/react-shim.js']);
+        assert.file(['src/styles.css']);
         assert.file(['static/manifest.json']);
+        assert.file(['static/images/icon@0.5x.png']);
         assert.file(['static/images/icon@1x.png']);
         assert.file(['static/images/icon@2x.png']);
         assert.file(['static/images/icon@3x.png']);
         assert.file(['static/images/icon@4x.png']);
-        assert.file(['package.json']);
-        assert.file(['README.md']);
-        assert.file(['src/main.jsx']);
       });
   });
 
@@ -46,14 +61,43 @@ describe('generator-xd-plugin:app', () => {
       .withPrompts({ name: answers.projectDisplayName })
       .withPrompts({ scriptType: answers.framework.plain.dialog })
       .then(() => {
+        assert.file(['.editorconfig']);
+        assert.file(['.eslintignore']);
+        assert.file(['.eslintrc']);
+        assert.file(['.gitignore']);
+        assert.file(['README.md']);
+        assert.file(['package.json']);
+        assert.file(['webpack.config.js']);
+        assert.file(['src/main.js']);
         assert.file(['static/manifest.json']);
+        assert.file(['static/images/icon@0.5x.png']);
         assert.file(['static/images/icon@1x.png']);
         assert.file(['static/images/icon@2x.png']);
         assert.file(['static/images/icon@3x.png']);
         assert.file(['static/images/icon@4x.png']);
-        assert.file(['package.json']);
+      });
+  });
+
+  it('generating template of plugin with panel ui with plain js', () => {
+    return helpers
+      .run(path.join(__dirname, '../generators/app'))
+      .withPrompts({ name: answers.projectDisplayName })
+      .withPrompts({ scriptType: answers.framework.panelplain.dialog })
+      .then(() => {
+        assert.file(['.editorconfig']);
+        assert.file(['.eslintignore']);
+        assert.file(['.eslintrc']);
+        assert.file(['.gitignore']);
         assert.file(['README.md']);
+        assert.file(['package.json']);
+        assert.file(['webpack.config.js']);
         assert.file(['src/main.js']);
+        assert.file(['static/manifest.json']);
+        assert.file(['static/images/icon@0.5x.png']);
+        assert.file(['static/images/icon@1x.png']);
+        assert.file(['static/images/icon@2x.png']);
+        assert.file(['static/images/icon@3x.png']);
+        assert.file(['static/images/icon@4x.png']);
       });
   });
 });
